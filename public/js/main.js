27dd3bd3081596e5cc69a8921a49c6fa22998c63
Haseebk0678 <haseebkhan0678@gmail.com>
2022-08-06 23:37:15 -0400

    Revert "added language detector"
    This reverts commit 5c6834fc263d5ff14d6764248089147820eafae3.

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,6 @@ const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
-const lngDetector = new (require('languagedetect'));
-
 
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
@@ -20,6 +18,7 @@ socket.on('roomUsers', ({ room, users }) => {
   outputRoomName(room);
   outputUsers(users);
 });
+
 // Message from server
 socket.on('message', (message) => {
   console.log(message);
@@ -43,16 +42,8 @@ chatForm.addEventListener('submit', (e) => {
   }
 
   // Emit message to server
-  // Check message Confidence HERE
-  // let languageDetected = lngDetector.detect('This is a test.', 1)[0][0];
-  // let confidenceI = lngDetector.detect('This is a test.', 1)[0][1];
- // if(confidenceI > .5){
-  //   socket.emit('chatMessage', msg);
-  // }else{
-  //   socket.emit('chatMessage', `Can not detect language`)
-  // }
-
   socket.emit('chatMessage', msg);
+
   // Clear input
   e.target.elements.msg.value = '';
   e.target.elements.msg.focus();
